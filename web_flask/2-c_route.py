bf05d4ed90980590066340c3271aa0049fa6add8
angellalao <6369@holbertonstudents.com>
2023-08-14 18:51:29 -0700

    add route with text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that starts a flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """returns message to be displayed in users browser"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns message to be displayed in users browser"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """returns message to be displayed in users browser"""
+    return "C %s" % text
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
