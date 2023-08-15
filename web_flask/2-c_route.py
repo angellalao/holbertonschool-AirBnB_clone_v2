27615344c74a6d803a8891549619417ffc39c781
angellalao <6369@holbertonstudents.com>
2023-08-14 19:30:56 -0700

    pycodestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,21 +4,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """returns message to be displayed in users browser"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """returns message to be displayed in users browser"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def text(text):
     """returns message to be displayed in users browser"""
     formatted_text = text.replace("_", " ")
     return "C %s" % formatted_text
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
