84b70ed77a1a25f19dc34937f6a18e4eab56df02
angellalao <6369@holbertonstudents.com>
2023-08-18 00:20:36 -0700

    task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""script that starts a flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def states_list():
+    """returns list of state objects present in db storage"""
+    states = storage.all(State)
+    print(states)
+    print({type(states)})
+    for v in states.values():
+        print(v)
+        print({type(v)})
+    cities = storage.all(City)
+    print(cities)
+    for v in cities.values():
+        print(v)
+        print({type(v)})
+    return render_template("8-cities_by_states.html", states=states, \
+                           cities=cities)
+
+
+@app.teardown_appcontext
+def app_teardown(exception):
+    """removes current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
