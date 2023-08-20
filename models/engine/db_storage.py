43f9e3166fdadb7069c6110485b8c823a3c69a02
angellalao <6369@holbertonstudents.com>
2023-08-20 06:32:21 -0700

    edit commebt

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,7 +7,7 @@ import os
 from models.user import User
 from models.state import State
 from models.city import City
-#from models.place import Place
+# from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
